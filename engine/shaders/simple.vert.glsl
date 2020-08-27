9a058edb5b6bf0976e8b14f8537ecda8dcf0d118
Robin Kertels <robin.kertels@gmail.com>
2020-08-27 13:11:24 +0200

    Modify simple shaders to match textured vertex

--- engine/shaders/simple.vert.glsl
+++ engine/shaders/simple.vert.glsl
@@ -3,10 +3,13 @@
 
 layout(location = 0) in vec3 in_pos;
 layout(location = 1) in vec3 in_color;
+layout(location = 2) in vec2 in_uv;
 
 layout(location = 0) out vec3 out_color;
+layout(location = 1) out vec2 out_uv;
 
 void main(void) {
   out_color = in_color;
+  out_uv = in_uv;
   gl_Position = vec4(in_pos, 1);
 }
