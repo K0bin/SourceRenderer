93c6181b2fbb4fb8da5eb83412877e68126606dd
Robin Kertels <robin.kertels@gmail.com>
2021-01-11 17:46:19 +0100

    Reduce amount of generated verts and generate UVs

--- engine/src/asset/loaders/bsp/bsp_level.rs
+++ engine/src/asset/loaders/bsp/bsp_level.rs
@@ -1,10 +1,10 @@
 
-use sourcerenderer_core::{Platform, Quaternion, Vec4};
+use sourcerenderer_core::{Platform, Quaternion, Vec4, Vec2I};
 use crate::asset::{AssetLoader, AssetType, Asset, Mesh, Model, AssetManager};
 use std::fs::File;
 use std::path::Path;
 use std::sync::Arc;
-use sourcerenderer_bsp::{Map, Node, Leaf, SurfaceEdge, LeafBrush, LeafFace, Vertex, Face, Edge, Plane, TextureData, TextureInfo, TextureStringData, TextureDataStringTable, BrushModel, DispVert, DispTri, DispInfo};
+use sourcerenderer_bsp::{Map, Node, Leaf, SurfaceEdge, LeafBrush, LeafFace, Vertex, Face, Edge, Plane, TextureData, TextureInfo, TextureStringData, TextureDataStringTable, BrushModel, DispVert, DispTri, DispInfo, NeighborEdge, DispSubNeighbor};
 use std::sync::Mutex;
 use std::collections::HashMap;
 use sourcerenderer_core::{Vec3, Vec2};
@@ -22,6 +22,8 @@ use std::io::Cursor;
 use std::collections::HashSet;
 use crate::asset::loaders::vpk_container::new_vpk_container;
 use crate::asset::loaders::PakFileContainer;
+use super::BspLumps;
+use std::cell::RefCell;
 
 // REFERENCE
 // https://github.com/lewa-j/Unity-Source-Tools/blob/1c5dc0635cdc4c65775d4af2c4449be49639f46b/Assets/Code/Read/SourceBSPLoader.cs#L877
@@ -29,30 +31,10 @@ use crate::asset::loaders::PakFileContainer;
 // https://github.com/Metapyziks/SourceUtils/blob/master/SourceUtils.WebExport/Bsp/Geometry.cs
 // http://web.archive.org/web/20050426034532/http://www.geocities.com/cofrdrbob/bspformat.html
 // https://github.com/toji/webgl-source/blob/a435841a856bb3d43f9783d3d2e7ac1cb63992a5/js/source-bsp.js
+// https://github.com/Galaco/kero/blob/dev/LICENSE
 
 pub struct BspLevelLoader {
   map_name_regex: Regex
-}
-
-struct BspTemp {
-  map: Map<BufReader<File>>,
-  map_name: String,
-  leafs: Vec<Leaf>,
-  nodes: Vec<Node>,
-  leaf_faces: Vec<LeafFace>,
-  leaf_brushes: Vec<LeafBrush>,
-  surface_edges: Vec<SurfaceEdge>,
-  vertices: Vec<Vertex>,
-  faces: Vec<Face>,
-  edges: Vec<Edge>,
-  planes: Vec<Plane>,
-  tex_data: Vec<TextureData>,
-  tex_info: Vec<TextureInfo>,
-  tex_string_data: TextureStringData,
-  tex_data_string_table: Vec<TextureDataStringTable>,
-  disp_infos: Vec<DispInfo>,
-  disp_verts: Vec<DispVert>,
-  disp_tris: Vec<DispTri>
 }
 
 const SCALING_FACTOR: f32 = 0.0236f32;
@@ -64,36 +46,7 @@ impl BspLevelLoader {
     }
   }
 
-  fn read_node(&self, node: &Node, temp: &BspTemp, brush_vertices: &mut Vec<crate::Vertex>, brush_indices: &mut HashMap<String, Vec<u32>>) {
-    let left_child = node.children[0];
-    self.read_child(left_child, temp, brush_vertices, brush_indices);
-    let right_child = node.children[1];
-    self.read_child(right_child, temp, brush_vertices, brush_indices);
-  }
-
-  fn read_child(&self, index: i32, temp: &BspTemp, brush_vertices: &mut Vec<crate::Vertex>, brush_indices: &mut HashMap<String, Vec<u32>>) {
-    if index < 0 {
-      self.read_leaf(&temp.leafs[(-1 - index) as usize], temp, brush_vertices, brush_indices);
-    } else {
-      self.read_node(&temp.nodes[index as usize], temp, brush_vertices, brush_indices);
-    };
-  }
-
-  fn read_leaf(&self, leaf: &Leaf, temp: &BspTemp, brush_vertices: &mut Vec<crate::Vertex>, brush_indices: &mut HashMap<String, Vec<u32>>) {
-    for leaf_face_index in leaf.first_leaf_face as u32 .. leaf.first_leaf_face as u32 + leaf.leaf_faces_count as u32 {
-      let face_index = temp.leaf_faces[leaf_face_index as usize].index;
-      let face = &temp.faces[face_index as usize];
-
-      let disp_info = if face.displacement_info != -1 { Some(&temp.disp_infos[face.displacement_info as usize]) } else { None };
-      if let Some(disp_info) = disp_info {
-        self.build_displacement_face(temp, disp_info, brush_vertices, brush_indices);
-      } else {
-        self.build_face(temp, face, brush_vertices, brush_indices);
-      }
-    }
-  }
-
-  fn build_face(&self, temp: &BspTemp, face: &Face, brush_vertices: &mut Vec<crate::Vertex>, brush_indices: &mut HashMap<String, Vec<u32>>) {
+  fn build_face(&self, temp: &BspLumps, face: &Face, brush_vertices: &mut Vec<crate::Vertex>, brush_indices: &mut HashMap<String, Vec<u32>>) {
     let tex_info = &temp.tex_info[face.texture_info as usize];
     let tex_data = &temp.tex_data[tex_info.texture_data as usize];
     let tex_offset = &temp.tex_data_string_table[tex_data.name_string_table_id as usize];
@@ -126,7 +79,11 @@ impl BspLevelLoader {
     }
   }
 
-  fn build_displacement_face(&self, temp: &BspTemp, disp_info: &DispInfo, brush_vertices: &mut Vec<crate::Vertex>, brush_indices: &mut HashMap<String, Vec<u32>>) {
+  fn build_displacement_face(&self,
+                             temp: &BspLumps,
+                             disp_info: &DispInfo,
+                             brush_vertices: &mut Vec<crate::Vertex>,
+                             brush_indices: &mut HashMap<String, Vec<u32>>) {
     let face = &temp.faces[disp_info.map_face as usize];
     let tex_info = &temp.tex_info[face.texture_info as usize];
     let tex_data = &temp.tex_data[tex_info.texture_data as usize];
@@ -135,17 +92,19 @@ impl BspLevelLoader {
     let plane = &temp.planes[face.plane_index as usize];
     let material_brush_indices = &mut brush_indices.entry(tex_name.clone()).or_default();
 
-    let disp_plane = &temp.planes[face.plane_index as usize];
     let mut corners = [Vec3::default(); 4];
-    let size = 1 << disp_info.power;
+    let mut corners_uv = [Vec2::default(); 4];
     let mut first_corner = 0;
     let mut first_corner_dist_squared = f32::MAX;
     for surf_edge_index in face.first_edge..face.first_edge + face.edges_count as i32 {
+
       let edge_index = temp.surface_edges[surf_edge_index as usize].index;
       let edge = temp.edges[edge_index.abs() as usize];
       let vert_index = edge.vertex_index[if edge_index >= 0 { 0 } else { 1 }];
       let position = temp.vertices[vert_index as usize].position;
-      corners[(surf_edge_index - face.first_edge) as usize] = position;
+      let index = (surf_edge_index - face.first_edge) as usize;
+      corners[index] = position;
+      corners_uv[index] = Self::calculate_uv(&position, &tex_info.texture_vecs_s, &tex_info.texture_vecs_t, &tex_data);
 
       let dist_squared = (disp_info.start_position - position).magnitude_squared();
       if dist_squared < first_corner_dist_squared {
@@ -154,42 +113,43 @@ impl BspLevelLoader {
       }
     }
 
-    for x in 0..size {
-      for y in 0..size {
-        let root_vertex = brush_vertices.len() as u32;
+    let subdivisions = 1 << disp_info.power;
+    let size = subdivisions + 1;
+    let sub_div_mul = 1f32 / (subdivisions as f32);
+    for y in 0..subdivisions {
+      let old_len = brush_vertices.len() as u32;
+      let v = [
+        y as f32 * sub_div_mul,
+        (y + 1) as f32 * sub_div_mul
+      ];
+      for x in 0..size {
+        let u = x as f32 * sub_div_mul;
 
         brush_vertices.push(crate::Vertex {
           position: Self::calculate_disp_vert(disp_info.disp_vert_start, x, y, size, &corners, first_corner, &temp.disp_verts),
           normal: Self::fixup_normal(&plane.normal),
           color: Vec3::new(1.0f32, 1.0f32, 1.0f32),
-          uv: Self::calculate_disp_uv(x, y, size, &face)
+          uv: (corners_uv[0] * (1f32 - u) + corners_uv[3] * u) * (1f32 - v[0]) + (corners_uv[1] * (1f32 - u) + corners_uv[2] * u) * v[0]
         });
+
+        if brush_vertices.len() - old_len as usize >= 3 {
+          material_brush_indices.push(brush_vertices.len() as u32 - 3);
+          material_brush_indices.push(brush_vertices.len() as u32 - 1);
+          material_brush_indices.push(brush_vertices.len() as u32 - 2);
+        }
+
         brush_vertices.push(crate::Vertex {
           position: Self::calculate_disp_vert(disp_info.disp_vert_start, x, y + 1, size, &corners, first_corner, &temp.disp_verts),
           normal: Self::fixup_normal(&plane.normal),
           color: Vec3::new(1.0f32, 1.0f32, 1.0f32),
-          uv: Self::calculate_disp_uv(x, y + 1, size, &face)
-        });
-        brush_vertices.push(crate::Vertex {
-          position: Self::calculate_disp_vert(disp_info.disp_vert_start, x + 1, y + 1, size, &corners, first_corner, &temp.disp_verts),
-          normal: Self::fixup_normal(&plane.normal),
-          color: Vec3::new(1.0f32, 1.0f32, 1.0f32),
-          uv: Self::calculate_disp_uv(x + 1, y + 1, size, &face)
-        });
-        brush_vertices.push(crate::Vertex {
-          position: Self::calculate_disp_vert(disp_info.disp_vert_start, x + 1, y, size, &corners, first_corner, &temp.disp_verts),
-          normal: Self::fixup_normal(&plane.normal),
-          color: Vec3::new(1.0f32, 1.0f32, 1.0f32),
-          uv: Self::calculate_disp_uv(x + 1, y, size, &face)
+          uv: (corners_uv[0] * (1f32 - u) + corners_uv[3] * u) * (1f32 - v[1]) + (corners_uv[1] * (1f32 - u) + corners_uv[2] * u) * v[1]
         });
 
-        material_brush_indices.push(root_vertex);
-        material_brush_indices.push(root_vertex + 2);
-        material_brush_indices.push(root_vertex + 1);
-
-        material_brush_indices.push(root_vertex);
-        material_brush_indices.push(root_vertex + 3);
-        material_brush_indices.push(root_vertex + 2);
+        if brush_vertices.len() - old_len as usize >= 3 {
+          material_brush_indices.push(brush_vertices.len() as u32 - 3);
+          material_brush_indices.push(brush_vertices.len() as u32 - 2);
+          material_brush_indices.push(brush_vertices.len() as u32 - 1);
+        }
       }
     }
   }
@@ -199,9 +159,9 @@ impl BspLevelLoader {
   }
 
   fn calculate_disp_vert(offset: i32, x: i32, y: i32, size: i32, corners: &[Vec3; 4], first_corner: i32, disp_verts: &[DispVert]) -> Vec3 {
-    let disp_vert = &disp_verts[(offset + x + y * (size + 1)) as usize];
-    let tx = (x as f32) / (size as f32);
-    let ty = (y as f32) / (size as f32);
+    let disp_vert = &disp_verts[(offset + x + y * size) as usize];
+    let tx = (x as f32) / ((size - 1) as f32);
+    let ty = (y as f32) / ((size - 1) as f32);
     let sx = 1f32 - tx;
     let sy = 1f32 - ty;
 
@@ -266,7 +226,7 @@ impl<P: Platform> AssetLoader<P> for BspLevelLoader {
     let disp_tris = map.read_disp_tris().unwrap();
     let mut pakfile = map.read_pakfile().unwrap();
 
-    let temp = BspTemp {
+    let temp = BspLumps {
       map,
       map_name: name.to_string(),
       nodes,
@@ -300,13 +260,11 @@ impl<P: Platform> AssetLoader<P> for BspLevelLoader {
 
     for model in &brush_models {
       let root = &temp.nodes[model.head_node as usize];
-      //self.read_node(root, &temp, &mut brush_vertices, &mut per_material_indices);
 
       for face in &temp.faces[model.first_face as usize .. (model.first_face + model.num_faces) as usize] {
         if face.displacement_info != -1 {
-          let displacement = &temp.disp_infos[face.displacement_info as usize];
-          //self.build_face(&temp, &temp.faces[displacement.map_face as usize], &mut brush_vertices, &mut per_material_indices);
-          self.build_displacement_face(&temp, displacement, &mut brush_vertices, &mut per_material_indices);
+          let disp_info = &temp.disp_infos[face.displacement_info as usize];
+          self.build_displacement_face(&temp, disp_info, &mut brush_vertices, &mut per_material_indices);
         } else {
           self.build_face(&temp, face, &mut brush_vertices, &mut per_material_indices);
         }
---
+++ engine/src/asset/loaders/bsp/mod.rs
@@ -0,0 +1,5 @@
+mod bsp_lumps;
+mod bsp_level;
+
+pub use bsp_level::BspLevelLoader;
+use bsp_lumps::BspLumps;
