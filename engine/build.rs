9c0beb67c168eca501b373c51e92420e51c774a6
Robin Kertels <robin.kertels@gmail.com>
2022-02-20 02:10:16 +0100

    Support compiling ray tracing shaders

--- engine/build.rs
+++ engine/build.rs
@@ -16,9 +16,16 @@ fn main() {
     .for_each(|file| {
       println!("cargo:rerun-if-changed={}", file.path().as_path().to_str().unwrap());
 
+      let mut is_rt = false;
       let path = file.path();
+      if let Some(path) = path.to_str() {
+        is_rt = path.contains(".rchit") || path.contains("rgen") || path.contains(".rmiss");
+      }
+
       let compiled_file_path = Path::join(Path::new(&shader_dir), [path.file_stem().unwrap().to_str().unwrap(), ".spv"].concat());
       let output = Command::new("glslangValidator")
+      .arg("--target-env")
+      .arg(if is_rt { "spirv1.4" } else { "spirv1.3" })
       .arg("-V")
       .arg("-o")
       .arg(compiled_file_path)
@@ -27,12 +34,7 @@ fn main() {
       .unwrap_or_else(|e| panic!("Failed to compile shader: {}\n{}", path.to_str().unwrap(), e.to_string()));
 
       if !output.status.success() {
-        let e = String::from_utf8(output.stdout);
-        if let Ok(e) = e {
-          panic!("Failed to compile shader: {}\n{}\n", path.to_str().unwrap(), e);
-        } else {
-          panic!("Failed to compile shader: {}", path.to_str().unwrap());
-        }
+        panic!("Failed to compile shader: {}\n{:?}\n", path.to_str().unwrap(), output);
       }
     }
   );
