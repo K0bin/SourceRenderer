a3bdcaede5a4d9d7bc45a77f4188d2de490c31ad
Robin Kertels <robin.kertels@gmail.com>
2022-05-22 01:08:49 +0200

    Handle weird _depth_X material paths

--- valve_formats/bsp/src/lump_data/pakfile.rs
+++ valve_formats/bsp/src/lump_data/pakfile.rs
@@ -1,16 +1,26 @@
 use zip::ZipArchive;
-use std::io::Cursor;
+use std::{io::Cursor, collections::HashMap};
 use crate::RawDataRead;
 
 pub struct PakFile {
-  archive: ZipArchive<Cursor<Box<[u8]>>>
+  archive: ZipArchive<Cursor<Box<[u8]>>>,
+  depth_file_names: HashMap<String, String>,
 }
 
 impl PakFile {
   pub(crate) fn new(data: Box<[u8]>) -> Self {
     let archive = ZipArchive::new(Cursor::new(data)).unwrap();
+    let file_names: Vec<String> = archive.file_names().map(|s| s.to_string()).collect();
+    let mut depth_file_names = HashMap::<String, String>::new();
+    for file_name in file_names {
+      let depth_index = file_name.rfind("_depth_");
+      if let Some(depth_index) = depth_index {
+        depth_file_names.insert(file_name[..depth_index].to_string(), file_name.to_string() + ".vmt");
+      }
+    }
     Self {
-      archive
+      archive,
+      depth_file_names,
     }
   }
 
@@ -18,7 +28,12 @@ impl PakFile {
     let lower_case_name = name.to_lowercase();
     let mut actual_name = Option::<String>::None;
     for zip_file_name in self.archive.file_names() {
-      if zip_file_name.to_lowercase() == lower_case_name {
+      if zip_file_name == lower_case_name {
+        actual_name = Some(zip_file_name.to_string());
+      }
+    }
+    for (file_name_without_depth, zip_file_name) in &self.depth_file_names {
+      if file_name_without_depth.as_str() == lower_case_name {
         actual_name = Some(zip_file_name.to_string());
       }
     }
