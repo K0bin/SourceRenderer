1a40797638159e9eb070d114368661603d4e2f29
Robin Kertels <robin.kertels@gmail.com>
2022-05-25 16:17:21 +0200

    Always do WASM release builds
    The debug ones are frustratingly slow.

--- platform/web/www/build.sh
+++ platform/web/www/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Build Rust code
-cd ../lib
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' wasm-pack build --target no-modules --dev -- -Z build-std=std,panic_abort
-cd ../www
+RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' wasm-pack build --target no-modules --release ../lib -Z build-std=std,panic_abort
 cp ../lib/pkg/sourcerenderer_web_bg.wasm dist/libsourcerenderer.wasm
 cp ../lib/pkg/sourcerenderer_web.js dist/libsourcerenderer_glue.js
 
