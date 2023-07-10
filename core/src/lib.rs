6c9db82020689945fd692c42aa192862783503d2
Robin Kertels <robin.kertels@gmail.com>
2023-07-10 15:24:12 +0200

    fixup: core add vec3ui

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -33,5 +33,6 @@ pub type Vec3 = nalgebra::Vector3<f32>;
 pub type Vec4 = nalgebra::Vector4<f32>;
 pub type Vec2I = nalgebra::Vector2<i32>;
 pub type Vec2UI = nalgebra::Vector2<u32>;
+pub type Vec3UI = nalgebra::Vector3<u32>;
 pub type Quaternion = nalgebra::UnitQuaternion<f32>;
 pub type Matrix4 = nalgebra::Matrix4<f32>;
