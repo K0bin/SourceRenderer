1b27321d9032bc84bcffc7a43ab237f1bcbe6af2
Robin Kertels <robin.kertels@gmail.com>
2023-07-30 22:08:05 +0200

    submission const

--- core/src/gpu/queue.rs
+++ core/src/gpu/queue.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 pub struct Submission<'a, B: GPUBackend> {
-  pub command_buffers: &'a mut [&'a mut B::CommandBuffer],
+  pub command_buffers: &'a [&'a B::CommandBuffer],
   pub wait_fences: &'a [FenceValuePairRef<'a, B>],
   pub signal_fences: &'a [FenceValuePairRef<'a, B>],
   pub acquire_swapchain: Option<&'a B::Swapchain>,
@@ -10,6 +10,6 @@ pub struct Submission<'a, B: GPUBackend> {
 
 pub trait Queue<B: GPUBackend> {
   unsafe fn create_command_pool(&self, command_pool_type: CommandPoolType, flags: CommandPoolFlags) -> B::CommandPool;
-  unsafe fn submit(&self, submissions: &mut [Submission<B>]);
+  unsafe fn submit(&self, submissions: &[Submission<B>]);
   unsafe fn present(&self, swapchain: &B::Swapchain);
 }
