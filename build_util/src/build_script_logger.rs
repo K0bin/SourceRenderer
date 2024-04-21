29a6d0efcc2381d536e0ba569196c1e009e1e0c8
Robin Kertels <robin.kertels@gmail.com>
2024-04-22 01:06:18 +0200

    Implement build script logger

---
+++ build_util/src/build_script_logger.rs
@@ -0,0 +1,25 @@
+use log::Log;
+
+struct BuildScriptLogger {}
+
+impl Log for BuildScriptLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        if !self.enabled(record.metadata()) { return; }
+        println!("cargo::warning=\"{}\"", record.args());
+    }
+
+    fn flush(&self) {}
+}
+
+pub fn init() {
+    let boxed = Box::new(BuildScriptLogger{});
+    unsafe {
+        let ptr = Box::into_raw(boxed);
+        log::set_logger(&*ptr).unwrap();
+        log::set_max_level(log::LevelFilter::Info);
+    }
+}
