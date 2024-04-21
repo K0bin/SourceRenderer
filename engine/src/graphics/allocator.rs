2424d28dd13152560d7f5102dd9f89127288c825
Robin Kertels <robin.kertels@gmail.com>
2024-04-21 20:15:55 +0200

    fix allocator

--- engine/src/graphics/allocator.rs
+++ engine/src/graphics/allocator.rs
@@ -1,10 +1,12 @@
-use std::{sync::Arc, sync::Mutex};
+use std::sync::{atomic::AtomicU64, Arc, Mutex};
 
 use smallvec::SmallVec;
 
 use super::align_up_64;
 
 // TODO: Implement Two Level Seggregate Fit allocator
+
+const DEBUG: bool = false;
 
 pub(super) struct Chunk<T>
     where T : Send + Sync
@@ -17,7 +19,9 @@ struct ChunkInner<T>
 where T : Send + Sync {
     free_list: Mutex<SmallVec<[Range; 16]>>,
     data: T,
-    free_callback: Option<Box<dyn Fn(&[Range]) + Send + Sync>>
+    free_callback: Option<Box<dyn Fn(&[Range]) + Send + Sync>>,
+
+    debug_offset: AtomicU64
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -71,7 +75,8 @@ impl<T> Chunk<T>
             inner: Arc::new(ChunkInner {
                 free_list: Mutex::new(free_list),
                 data,
-                free_callback: None
+                free_callback: None,
+                debug_offset: AtomicU64::new(0u64)
             }),
             size: chunk_size
         }
@@ -88,7 +93,8 @@ impl<T> Chunk<T>
             inner: Arc::new(ChunkInner {
                 free_list: Mutex::new(free_list),
                 data,
-                free_callback: Some(Box::new(free_callback))
+                free_callback: Some(Box::new(free_callback)),
+                debug_offset: AtomicU64::new(0u64)
             }),
             size: chunk_size
         }
@@ -160,22 +166,16 @@ impl<T> Chunk<T>
             if consume_entire_range {
                 free_list.remove(free_index);
             } else {
-                let alignment_mod = range.offset % alignment;
-                let alignment_diff = (alignment - alignment_mod) % alignment;
-
-                if range.length != size + alignment_diff {
-                    let existing_range = &mut free_list[free_index];
-                    existing_range.offset += alignment_diff;
-                    existing_range.length -= alignment_diff;
-                }
+                let existing_range = &mut free_list[free_index];
+                debug_assert!(existing_range.length > size + alignment_diff);
+                existing_range.offset += size + alignment_diff;
+                existing_range.length -= size + alignment_diff;
             }
+
             Allocation {
                 inner: self.inner.clone(),
                 data_ptr: &self.inner.data as *const T,
-                range: Range {
-                    offset: range.offset,
-                    length: size
-                }
+                range
             }
         })
     }
@@ -188,18 +188,32 @@ impl<T> Chunk<T>
         let first = free_list.first().unwrap();
         first.offset == 0 && first.length == self.size
     }
+
+    pub fn size(&self) -> u64 {
+        self.size
+    }
 }
 
 impl<T> Drop for Allocation<T>
     where T : Send + Sync
 {
     fn drop(&mut self) {
+        if DEBUG {
+            return;
+        }
+
         let mut free_list = self.inner.free_list.lock().unwrap();
 
+        let mut insert_position = Option::<usize>::None;
         let mut i = 0usize;
         while i < free_list.len() {
             let drop_i = {
                 let range = &free_list[i];
+
+                if range.offset > self.range.offset + self.range.length {
+                    insert_position = Some(i);
+                    break;
+                }
 
                 if range.offset == self.range.offset + self.range.length {
                     self.range.length += range.length;
@@ -219,7 +233,11 @@ impl<T> Drop for Allocation<T>
             }
         }
 
-        free_list.push(self.range.clone());
+        if let Some(insert_position) = insert_position {
+            free_list.insert(insert_position, self.range.clone());
+        } else {
+            free_list.push(self.range.clone());
+        }
         if let Some(callback) = self.inner.free_callback.as_ref() {
             callback(&free_list[..]);
         }
