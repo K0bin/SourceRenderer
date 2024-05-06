1d07ce038cf2b7fcb917b6a357d8265138b176f4
Robin Kertels <robin.kertels@gmail.com>
2024-05-07 01:35:45 +0200

    Fix storage textures not being marked as writable

--- core/src/gpu/texture.rs
+++ core/src/gpu/texture.rs
@@ -14,7 +14,7 @@ bitflags! {
     const BLIT_DST      = 0b100000000;
     const DEPTH_STENCIL = 0b1000000000;
 
-    const GPU_WRITABLE = 0b10 | 0b10000 | 0b1000000 | 0b100000000 | 0b1000000000;
+    const GPU_WRITABLE = 0b10 | 0b100 | 0b10000 | 0b1000000 | 0b100000000 | 0b1000000000;
   }
 }
 
