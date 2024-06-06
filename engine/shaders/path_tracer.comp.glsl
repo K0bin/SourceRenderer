23ebc432dd92cd3ce938d6445caadf2de9f70f72
Robin Kertels <robin.kertels@gmail.com>
2024-06-06 16:49:54 +0200

    Begin implementing path tracing with multiple bounces

--- engine/shaders/path_tracer.comp.glsl
+++ engine/shaders/path_tracer.comp.glsl
@@ -29,6 +29,23 @@ layout(set = DESCRIPTOR_SET_FREQUENT, binding = 2) uniform sampler2D noise;
 layout(set = DESCRIPTOR_SET_VERY_FREQUENT, binding = 3) uniform sampler linearSampler;
 layout(set = DESCRIPTOR_SET_TEXTURES_BINDLESS, binding = 0) uniform texture2D albedo_global[1024];
 
+#define PI 3.1415926538
+#define SUN_ANGLE 0.53
+
+struct RayHitResult {
+    vec3 radiance;
+    vec3 nextRayOrigin;
+    vec3 nextRayDirection;
+    vec3 nextFactor;
+};
+
+const uint RAYS_PER_PIXEL = 300;
+
+vec3 random(uint iteration);
+bool traceRay(vec3 rayOrigin, vec3 rayDirection, uint iteration, out RayHitResult result);
+void rayHit(uint drawableIndex, uint partIndex, uint primitiveIndex, vec2 barycentricsYZ, uint iteration, out RayHitResult result);
+void rayMiss(vec3 rayDirection, out RayHitResult result);
+
 void main() {
     ivec2 texSize = imageSize(image);
     if (gl_GlobalInvocationID.x >= texSize.x || gl_GlobalInvocationID.y >= texSize.y) {
@@ -37,26 +54,36 @@ void main() {
     vec2 texCoord = vec2((float(gl_GlobalInvocationID.x) + 0.5) / float(texSize.x), (float(gl_GlobalInvocationID.y) + 0.5) / float(texSize.y));
     ivec2 iTexCoord = ivec2(gl_GlobalInvocationID.xy);
 
-
-    /*bindless debug
-    uint index = max(8, min(iTexCoord.x, 8));
-    vec3 color1 = texture(sampler2D(albedo_global[index], linearSampler), texCoord).xyz;
-    //vec3 color1 = texture(sampler2D(tex15, linearSampler), texCoord).xyz;
-    //vec3 color1 = vec3(1.0);
-    imageStore(image, iTexCoord, vec4(color1, 1.0));
-
-    return;*/
-
     vec3 rayOrigin = camera.position.xyz;
     vec4 rayDirectionViewSpace = camera.invProj * vec4(vec2(texCoord.x, 1.0 - texCoord.y) * 2.0 - 1.0, 1.0, 1.0);
     rayDirectionViewSpace.xyz /= rayDirectionViewSpace.w;
-    vec4 rayDirection = camera.invView * (rayDirectionViewSpace);
-    rayDirection = normalize(rayDirection);
+    vec4 cameraRayDirection = camera.invView * (rayDirectionViewSpace);
+    vec3 rayDirection = normalize(cameraRayDirection.xyz);
 
+    vec3 contribution = vec3(1.0);
+    vec3 color = vec3(0.0);
+    for (uint i = 0; i < RAYS_PER_PIXEL; i++) {
+        RayHitResult result;
+        bool hit = traceRay(rayOrigin, rayDirection.xyz, i, result);
+
+        color += contribution * result.radiance;
+        contribution *= result.nextFactor;
+
+        if (!hit || result.nextFactor.x + result.nextFactor.y + result.nextFactor.z <= 0.1) {
+            break;
+        }
+        rayOrigin = result.nextRayOrigin;
+        rayDirection = result.nextRayDirection;
+    }
+
+    imageStore(image, iTexCoord, vec4(color, 1));
+}
+
+bool traceRay(vec3 rayOrigin, vec3 rayDirection, uint iteration, out RayHitResult result) {
     rayQueryEXT rayQuery;
     rayQueryInitializeEXT(rayQuery, topLevelAS,
-                      0,
-                      0xFF, rayOrigin, 0.01, rayDirection.xyz, 100.0);
+                      gl_RayFlagsCullBackFacingTrianglesEXT,
+                      0xFF, rayOrigin, 0.01, rayDirection, 100.0);
 
     while (rayQueryProceedEXT(rayQuery)) {
         if (rayQueryGetIntersectionTypeEXT(rayQuery, false) ==
@@ -69,33 +96,82 @@ void main() {
     vec3 color;
     if (rayQueryGetIntersectionTypeEXT(rayQuery, true) ==
         gl_RayQueryCommittedIntersectionNoneEXT) {
-        color = vec3(0.0);
+        rayMiss(rayDirection, result);
+        return false;
     } else {
         vec2 barycentricsYZ = rayQueryGetIntersectionBarycentricsEXT(rayQuery, true);
         int drawableIndex = rayQueryGetIntersectionInstanceIdEXT(rayQuery, true);
         int partIndex = rayQueryGetIntersectionGeometryIndexEXT(rayQuery, true);
         int primitiveId = rayQueryGetIntersectionPrimitiveIndexEXT(rayQuery, true);
 
-        GPUDrawable drawable = GPU_SCENE_DRAWABLES_NAME[drawableIndex];
-        GPUMeshPart part = GPU_SCENE_PARTS_NAME[drawable.partStart + partIndex];
-
-        uint firstIndex = part.meshFirstIndex + primitiveId * 3;
-        uint index0 = INDICES_ARRAY_NAME[firstIndex];
-        uint index1 = INDICES_ARRAY_NAME[firstIndex + 1];
-        uint index2 = INDICES_ARRAY_NAME[firstIndex + 2];
-
-        Vertex triangle_verts[3];
-        triangle_verts[0] = VERTICES_ARRAY_NAME[part.meshVertexOffset + index0];
-        triangle_verts[1] = VERTICES_ARRAY_NAME[part.meshVertexOffset + index1];
-        triangle_verts[2] = VERTICES_ARRAY_NAME[part.meshVertexOffset + index2];
-
-        vec3 barycentrics = vec3(1.0 - barycentricsYZ.x - barycentricsYZ.y, barycentricsYZ.x, barycentricsYZ.y);
-        Vertex vertex = interpolateVertex(barycentrics, triangle_verts);
-
-        GPUMaterial material = GPU_SCENE_MATERIALS_NAME[part.materialIndex];
-        vec3 albedo = material.albedoColor.rgb * texture(sampler2D(albedo_global[material.albedoTextureIndex], linearSampler), vertex.uv).rgb;
-        color = albedo;
+        rayHit(drawableIndex, partIndex, primitiveId, barycentricsYZ, iteration, result);
+        return true;
     }
+}
 
-    imageStore(image, iTexCoord, vec4(color, 1));
+// vec3 pbr(vec3 lightDir, vec3 viewDir, vec3 normal, vec3 f0, vec3 albedo, vec3 radiance, float roughness, float metalness);
+
+void rayHit(uint drawableIndex, uint partIndex, uint primitiveIndex, vec2 barycentricsYZ, uint iteration, out RayHitResult result) {
+    GPUDrawable drawable = GPU_SCENE_DRAWABLES_NAME[drawableIndex];
+    GPUMeshPart part = GPU_SCENE_PARTS_NAME[drawable.partStart + partIndex];
+
+    uint firstIndex = part.meshFirstIndex + primitiveIndex * 3;
+    uint index0 = INDICES_ARRAY_NAME[firstIndex];
+    uint index1 = INDICES_ARRAY_NAME[firstIndex + 1];
+    uint index2 = INDICES_ARRAY_NAME[firstIndex + 2];
+
+    Vertex triangle_verts[3];
+    triangle_verts[0] = VERTICES_ARRAY_NAME[part.meshVertexOffset + index0];
+    triangle_verts[1] = VERTICES_ARRAY_NAME[part.meshVertexOffset + index1];
+    triangle_verts[2] = VERTICES_ARRAY_NAME[part.meshVertexOffset + index2];
+
+    vec3 barycentrics = vec3(1.0 - barycentricsYZ.x - barycentricsYZ.y, barycentricsYZ.x, barycentricsYZ.y);
+    Vertex vertex = interpolateVertex(barycentrics, triangle_verts);
+
+    GPUMaterial material = GPU_SCENE_MATERIALS_NAME[part.materialIndex];
+    vec3 albedo = material.albedoColor.rgb * texture(sampler2D(albedo_global[material.albedoTextureIndex], linearSampler), vertex.uv).rgb;
+    vec3 color = albedo;
+    vec3 emission = vec3(0.0);
+
+    vec3 random = random(iteration);
+    // uniform sampling of hemisphere
+    float theta = 0.5 * PI * random.y;
+    float phi = 2.0 * PI * random.x;
+
+    vec3 localDiffuseDir = vec3(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+    vec3 diffuseDir = vertex.normal * localDiffuseDir;
+    result.nextRayOrigin = vertex.position;
+    result.nextRayDirection = diffuseDir;
+    result.nextFactor = color * PI * cos(theta) * sin(theta);
+    result.radiance = emission;
+
+    // DEBUG
+    result.radiance = color;
+    result.nextFactor = vec3(0.0);
+}
+
+vec3 random(uint iteration) {
+    ivec2 texSize = imageSize(image);
+    vec2 texCoord = vec2((float(gl_GlobalInvocationID.x) + 0.5) / float(texSize.x), (float(gl_GlobalInvocationID.y) + 0.5) / float(texSize.y));
+    texCoord += vec2(float(iteration / 3) * 0.3, float(iteration / 5) * 0.3);
+    texCoord = mod(texCoord, vec2(1.0));
+
+    return texture(noise, texCoord).xyz;
+}
+
+void rayMiss(vec3 rayDirection, out RayHitResult result) {
+    vec3 sunDirection = vec3(0.1, -1, 0.1);
+    sunDirection = normalize(sunDirection);
+    float angle = acos(dot(sunDirection, rayDirection));
+    if (angle < SUN_ANGLE) {
+        result.nextFactor = vec3(0.0);
+        result.radiance = vec3(20.0);
+        result.nextRayDirection = vec3(0.0);
+        result.nextRayOrigin = vec3(0.0);
+    } else {
+        result.nextFactor = vec3(0.0);
+        result.radiance = vec3(0.0);
+        result.nextRayDirection = vec3(0.0);
+        result.nextRayOrigin = vec3(0.0);
+    }
 }
--- engine/shaders/pbr.inc.glsl
+++ engine/shaders/pbr.inc.glsl
@@ -5,15 +5,15 @@
 
 vec3 pbr(vec3 lightDir, vec3 viewDir, vec3 normal, vec3 f0, vec3 albedo, vec3 radiance, float roughness, float metalness);
 float distributionGGX(vec3 normal, vec3 halfway, float roughness);
-float schlickGGX(float nDotV, float roughness);
-float geometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float k);
+float schlickGGX(float nDotV, float roughness, bool direct);
+float geometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float k, bool direct);
 vec3 fresnelSchlick(float cosTheta, vec3 f0);
 
 vec3 pbr(vec3 lightDir, vec3 viewDir, vec3 normal, vec3 f0, vec3 albedo, vec3 radiance, float roughness, float metalness) {
   vec3 halfway = normalize(viewDir + lightDir);
 
   float ndf = distributionGGX(normal, halfway, roughness);
-  float g = geometrySmith(normal, viewDir, lightDir, roughness);
+  float g = geometrySmith(normal, viewDir, lightDir, roughness, false);
   vec3 f = fresnelSchlick(max(dot(halfway, viewDir), 0.0), f0);
 
   vec3 kS = f;
@@ -34,17 +34,22 @@ float distributionGGX(vec3 normal, vec3 halfway, float roughness) {
   return a2 / (PI * x * x);
 }
 
-float geometrySchlickGGX(float nDotV, float roughness) {
+float geometrySchlickGGX(float nDotV, float roughness, bool direct) {
   float r = roughness + 1.0;
-  float k = (r*r) / 8.0;
+  float k;
+  if (direct) {
+    k = (r*r) / 8.0;
+  } else {
+    k = (roughness * roughness) / 2;
+  }
   return nDotV / (nDotV * (1.0 - k) + k);
 }
 
-float geometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float roughness) {
+float geometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float roughness, bool direct) {
   float nDotV = max(dot(normal, viewDir), 0.0);
   float nDotL = max(dot(normal, lightDir), 0.0);
-  float ggx2 = geometrySchlickGGX(nDotV, roughness);
-  float ggx1 = geometrySchlickGGX(nDotL, roughness);
+  float ggx2 = geometrySchlickGGX(nDotV, roughness, direct);
+  float ggx1 = geometrySchlickGGX(nDotL, roughness, direct);
   return ggx1 * ggx2;
 }
 
