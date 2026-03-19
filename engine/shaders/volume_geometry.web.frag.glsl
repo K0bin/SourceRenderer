069e0c8d91433d2e34578260c0e872e0d96e8a13
Robin Kertels <robin.kertels@gmail.com>
2026-03-19 14:22:13 +0100

    Adjust coloring

--- engine/shaders/volume_geometry.web.frag.glsl
+++ engine/shaders/volume_geometry.web.frag.glsl
@@ -21,11 +21,8 @@ layout(set = DESCRIPTOR_SET_FRAME, binding = 0) uniform CameraUBO {
 layout (set = DESCRIPTOR_SET_FREQUENT, binding = 1) uniform sampler2D transferFunction;
 
 void main(void) {
-    //out_color = texture(albedo, in_uv);
-    //float density = 0.5 - 0.8 * cos(3.1425 * in_density / 0.15);
-    float density = min((in_density / 0.15) * 6.0 - 1.5, 0.8);
-    vec3 albedo = vec3(0.8, density, density);
-    //albedo = texture(transferFunction, vec2(max(0.0, in_density - 0.045) / 0.1, 0.75));
+    float colorComponent = min((in_density / 0.15) * 6.0 - 1.5, 0.7);
+    vec3 albedo = vec3(0.7, colorComponent, density);
     vec3 lightDir = normalize(vec3(0.1, 1.0, 0.1));
     vec3 viewDir = normalize(camera.position.xyz - in_worldPosition.xyz);
     out_color = vec4(vec3(0.1) + pbr(lightDir, viewDir, in_normal, vec3(0.025), albedo, vec3(15.0), 0.1, 0.8), 1.0);
