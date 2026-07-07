2fdbf7f75da152e54d12f75a06ea29a0c73379dc
Robin Kertels <robin.kertels@gmail.com>
2026-07-07 14:56:42 +0200

    Use Vulkan on MacOS

--- graphics/vulkan/src/instance.rs
+++ graphics/vulkan/src/instance.rs
@@ -18,6 +18,7 @@ const SURFACE_XCB_EXT_NAME: &str = "VK_KHR_xcb_surface";
 const SURFACE_XLIB_EXT_NAME: &str = "VK_KHR_xlib_surface";
 const SURFACE_ANDROID_EXT_NAME: &str = "VK_KHR_android_surface";
 const SURFACE_WIN32_EXT_NAME: &str = "VK_KHR_win32_surface";
+const SURFACE_MACOS_EXT_NAME: &str = "VK_EXT_metal_surface";
 const DEBUG_UTILS_EXT_NAME: &str = "VK_EXT_debug_utils";
 const VALIDATION_LAYER_NAME: &str = "VK_LAYER_KHRONOS_validation";
 
@@ -71,6 +72,7 @@ impl VkInstance {
             SURFACE_XLIB_EXT_NAME,
             SURFACE_ANDROID_EXT_NAME,
             SURFACE_WIN32_EXT_NAME,
+            SURFACE_MACOS_EXT_NAME,
         ];
         let mut supports_surface_extension = false;
         let mut supports_platform_surface_extension = false;
@@ -183,10 +185,14 @@ impl VkInstance {
 
             let physical_devices: Vec<vk::PhysicalDevice> =
                 raw.instance.enumerate_physical_devices().unwrap();
-            let adapters: Vec<VkAdapter> = physical_devices
+            let mut adapters: Vec<VkAdapter> = physical_devices
                 .into_iter()
                 .map(|phys_dev| VkAdapter::new(&raw, phys_dev))
                 .collect();
+
+            if adapters.len() > 1 {
+                adapters.remove(1);
+            }
 
             VkInstance { raw, adapters }
         }
