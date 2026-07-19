ac8a0472174f580c815c440086abebc04248b5c2
Robin Kertels <robin.kertels@gmail.com>
2026-07-19 20:14:04 +0200

    Add new vertex sharing within subgroup

--- engine/shaders/marching_cubes.comp.glsl
+++ engine/shaders/marching_cubes.comp.glsl
@@ -4,7 +4,15 @@
 #extension GL_EXT_scalar_block_layout : enable
 #extension GL_EXT_shader_explicit_arithmetic_types_float16 : enable
 
-#define CACHE
+#extension GL_KHR_shader_subgroup_basic : enable
+#extension GL_KHR_shader_subgroup_arithmetic : enable
+#extension GL_KHR_shader_subgroup_vote : enable
+#extension GL_KHR_shader_subgroup_ballot : enable
+
+
+#define GLOBAL_HASHMAP
+#define SUBGROUP_SHARING
+
 
 layout(local_size_x = 4, local_size_y = 4, local_size_z = 4) in;
 
@@ -105,7 +113,7 @@ vec3 calculateNormal(vec3 pos) {
 const uint HashmapEmptyKey = ~0u;
 const uint HashmapEmptyValue = ~0u;
 const uint HashmapCapacity = 400000u;
-const uint HashmapMaxProbing = 0u; // 0 to disable the limit.
+const uint HashmapMaxProbing = 0u; //99u; //0u; // 0 to disable the limit.
 
 uint hash(uint key) {
     uint hash = key;
@@ -165,29 +173,60 @@ uint hashmapLookup(uint key) {
 }
 
 
+uint vertexKeyFromIndexOffsets(uint idx1, uint idx2) {
+    uvec3 vertexPos1 = gl_GlobalInvocationID + indexOffset(idx1);
+    uvec3 vertexPos2 = gl_GlobalInvocationID + indexOffset(idx2);
+    uint vtxKey = vertexKey(vertexPos1, vertexPos2);
+    return vtxKey;
+}
+
+
+uint addVertex(vec4 vertexPos) {
+    uint index = atomicAdd(vertexCount, 1u);
+    vertices[index].pos = f16vec3(vertexPos.xyz);
+    return index;
+}
+
+
 uint calculateAndAddVertex(uint idx1, uint idx2) {
+#ifdef SUBGROUP_SHARING
+    return ~0u;
+#endif
+
     uvec3 vertexPos1 = gl_GlobalInvocationID + indexOffset(idx1);
     uvec3 vertexPos2 = gl_GlobalInvocationID + indexOffset(idx2);
 
     uint index;
 
-    #ifdef CACHE
-    uint cacheKey = vertexKey(vertexPos1, vertexPos2);
-    index = hashmapLookup(cacheKey);
+#ifdef GLOBAL_HASHMAP
+    uint vtxKey = vertexKey(vertexPos1, vertexPos2);
+    index = hashmapLookup(vtxKey);
     if (index == HashmapEmptyValue) {
-    #endif
+#endif
 
-    vec4 vertex = interpolateVertices(vertexPos1, vertexPos2) * vec4(scale, 1.0);
+    vec4 vertexPos = interpolateVertices(vertexPos1, vertexPos2) * vec4(scale, 1.0);
+    index = addVertex(vertexPos);
 
-    index = atomicAdd(vertexCount, 1u);
-    vertices[index].pos = f16vec3(vertex.xyz);
-
-    #ifdef CACHE
-    hashmapInsert(cacheKey, index);
+#ifdef GLOBAL_HASHMAP
+    hashmapInsert(vtxKey, index);
     }
-    #endif
+#endif
 
     return index;
+}
+
+
+vec4 vertexPosFromKey(uint vertexKey) {
+    uvec3 sizes = gl_NumWorkGroups * gl_WorkGroupSize * 2;
+
+    uvec3 pos = uvec3(vertexKey % sizes.x,
+        (vertexKey / sizes.x) % sizes.y,
+        vertexKey / (sizes.x * sizes.y));
+
+    uvec3 pos1 = pos / 2u;
+    uvec3 pos2 = pos1 + (pos % 2u);
+
+    return interpolateVertices(pos1, pos2) * vec4(scale, 1.0);
 }
 
 
@@ -195,7 +234,7 @@ void main() {
     uvec3 base = gl_GlobalInvocationID;
 
     ivec3 imgSize = imageSize(densityImage);
-    uint key = 0u;
+    uint voxelKey = 0u;
     for (uint z = 0u; z < 2u; z++) {
         for (uint y = 0u; y < 2u; y++) {
             for (uint x = 0u; x < 2u; x++) {
@@ -206,12 +245,12 @@ void main() {
                 bool inRange = gl_GlobalInvocationID.x < imgSize.x - 1u
                     && gl_GlobalInvocationID.y < imgSize.y - 1u
                     && gl_GlobalInvocationID.z < imgSize.z - 1u;
-                key |= ((value >= threshold && inRange) ? 1u : 0u) << index;
+                voxelKey |= ((value >= threshold && inRange) ? 1u : 0u) << index;
             }
         }
     }
 
-    if (key == 0u || key == 255u)
+    if (voxelKey == 0u || voxelKey == 255u)
         return;
 
     instanceCount = 1u;
@@ -219,37 +258,114 @@ void main() {
     vertexOffset = 0;
     firstInstance = 0u;
 
+    uint[12u] vertexKeys;
     uint[12u] cubeVertexIndices;
+
     for (uint i = 0u; i < 4u; i++) {
-        if ((edges[key] & (1u << i)) != 0u) {
+        if ((edges[voxelKey] & (1u << i)) != 0u) {
             uint idx1 = i;
             uint idx2 = (i + 1u) % 4u;
+
             cubeVertexIndices[i] = calculateAndAddVertex(idx1, idx2);
+
+            uint vtxKey = vertexKeyFromIndexOffsets(idx1, idx2);
+            vertexKeys[i] = vtxKey;
         } else {
             cubeVertexIndices[i] = 0u;
+            vertexKeys[i] = ~0u;
         }
-        if ((edges[key] & (16u << i)) != 0u) {
+        if ((edges[voxelKey] & (16u << i)) != 0u) {
             uint idx1 = i + 4u;
             uint idx2 = (i + 1u) % 4u + 4u;
+
             cubeVertexIndices[i + 4u] = calculateAndAddVertex(idx1, idx2);
+
+            uint vtxKey = vertexKeyFromIndexOffsets(idx1, idx2);
+            vertexKeys[i + 4u] = vtxKey;
         } else {
             cubeVertexIndices[i + 4u] = 0u;
+            vertexKeys[i + 4u] = ~0u;
         }
-        if ((edges[key] & (256u << i)) != 0u) {
+        if ((edges[voxelKey] & (256u << i)) != 0u) {
             uint idx1 = i;
             uint idx2 = i + 4u;
+
             cubeVertexIndices[i + 8u] = calculateAndAddVertex(idx1, idx2);
+
+            uint vtxKey = vertexKeyFromIndexOffsets(idx1, idx2);
+            vertexKeys[i + 8u] = vtxKey;
         } else {
             cubeVertexIndices[i + 8u] = 0u;
+            vertexKeys[i + 8u] = ~0u;
         }
     }
 
-    for (uint i = 0u; i < 16u && tris[key][i] != -1; i += 3u) {
+
+#ifdef SUBGROUP_SHARING
+    for (uint i = 0u; i < 12u; i++) {
+        cubeVertexIndices[i] = ~0u;
+    }
+
+    subgroupBarrier();
+
+    for (uint i = 0u; i < 12u; i++) {
+        uint keyAtI = vertexKeys[i];
+
+        while (true) {
+            uint refkey = subgroupMin(keyAtI);
+            if (refkey == ~0u)
+                break;
+
+            uint vertexIndex = ~0u;
+            for (uint j = 0u; j < 12u; j++) {
+                if (subgroupAny(vertexIndex != ~0u)) {
+                    vertexIndex = subgroupMin(vertexIndex);
+                    break;
+                }
+
+                uint keyAtJ = vertexKeys[j];
+                if (keyAtJ != refkey)
+                    continue;
+
+                vertexIndex = cubeVertexIndices[j];
+            }
+            if (keyAtI == refkey) {
+                if (vertexIndex != ~0u) {
+                    cubeVertexIndices[i] = vertexIndex;
+                } else {
+                    // Add vertex
+                    uint index;
+                    if (subgroupElect()) {
+                        #ifdef GLOBAL_HASHMAP
+                        index = hashmapLookup(refkey);
+                        if (index == HashmapEmptyValue) {
+                        #endif
+
+                        vec4 vertexPos = vertexPosFromKey(refkey);
+                        index = addVertex(vertexPos);
+
+                        #ifdef GLOBAL_HASHMAP
+                        hashmapInsert(refkey, index);
+                        }
+                        #endif
+                    }
+                    index = subgroupBroadcastFirst(index);
+
+                    cubeVertexIndices[i] = index;
+                }
+                keyAtI = ~0u;
+            }
+        }
+    }
+#endif
+
+
+    for (uint i = 0u; i < 16u && tris[voxelKey][i] != -1; i += 3u) {
         uint firstIndex = atomicAdd(indexCount, 3u);
 
-        uint index0 = cubeVertexIndices[tris[key][i + 0u]];
-        uint index1 = cubeVertexIndices[tris[key][i + 1u]];
-        uint index2 = cubeVertexIndices[tris[key][i + 2u]];
+        uint index0 = cubeVertexIndices[tris[voxelKey][i + 0u]];
+        uint index1 = cubeVertexIndices[tris[voxelKey][i + 1u]];
+        uint index2 = cubeVertexIndices[tris[voxelKey][i + 2u]];
 
         indices[firstIndex + 0u] = index0;
         indices[firstIndex + 1u] = index1;
